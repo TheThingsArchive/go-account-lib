eea1bf031217af1b131618793576ec552257cf22
Romeo Van Snick <romeovs@gmail.com>
2016-09-23 13:59:00 +0200

    Test scopes

---
+++ scope/scope_test.go
@@ -0,0 +1,25 @@
+// Copyright © 2016 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package scope
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/assertions"
+)
+
+func TestApp(t *testing.T) {
+	a := New(t)
+	a.So(App("foo"), ShouldEqual, "apps:foo")
+}
+
+func TestGateway(t *testing.T) {
+	a := New(t)
+	a.So(Gateway("foo"), ShouldEqual, "gateways:foo")
+}
+
+func TestComponent(t *testing.T) {
+	a := New(t)
+	a.So(Component("foo"), ShouldEqual, "components:foo")
+}
