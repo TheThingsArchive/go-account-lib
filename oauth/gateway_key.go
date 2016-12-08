1a96fb927c9a27389d6594deb8e36dd6db05c914
Romeo Van Snick <romeovs@gmail.com>
2016-12-08 10:12:41 +0100

    Add ExchangeGatewayKeyForToken

---
+++ oauth/gateway_key.go
@@ -0,0 +1,22 @@
+// Copyright © 2016 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package oauth
+
+import (
+	"fmt"
+
+	"github.com/TheThingsNetwork/go-account-lib/auth"
+	"github.com/TheThingsNetwork/go-account-lib/util"
+	"github.com/TheThingsNetwork/go-utils/log"
+	"golang.org/x/oauth2"
+)
+
+// ExchangeGatewayKeyForToken exchanges an application Access Key for an equivalent
+func (o *Config) ExchangeGatewayKeyForToken(gatewayID, gatewayKey string) (*oauth2.Token, error) {
+	var ctx log.Interface
+	strategy := auth.AccessKey(gatewayKey)
+	token := &oauth2.Token{}
+	err := util.GET(ctx, o.Server, strategy, fmt.Sprintf("/api/v2/gateways/%s/token", gatewayID), token)
+	return token, err
+}
