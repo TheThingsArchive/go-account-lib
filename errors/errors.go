dbe530533e53ed2dfc608e13eecc0e7fdcbb6da2
Romeo Van Snick <romeovs@gmail.com>
2017-01-20 15:51:58 +0100

    Add all error types

--- errors/errors.go
+++ errors/errors.go
@@ -9,11 +9,13 @@ import (
 
 // StatusCode gets the status code from an error, defaulting to 500
 func StatusCode(err error) int {
-	switch err.(type) {
+	switch t := err.(type) {
 	case util.HTTPError:
-		return err.Code
-	case oauth.Error:
-		return err.Code
+		return t.Code
+	case *util.HTTPError:
+		return t.Code
+	case *oauth.Error:
+		return t.Code
 	default:
 		return http.StatusInternalServerError
 	}
