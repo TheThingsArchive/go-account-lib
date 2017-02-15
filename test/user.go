bb0f6cf4373faa5da7fe6ea025f683283fde5b8a
Roman Volosatovs <rvolosatovs@riseup.net>
2017-02-15 21:52:31 +0100

    test: fix user token generation

--- test/user.go
+++ test/user.go
@@ -4,48 +4,49 @@
 package test
 
 import (
+	"github.com/TheThingsNetwork/go-account-lib/scope"
 	"github.com/TheThingsNetwork/ttn/core/types"
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
 // UserClaims creates user claims for top level scopes (like apps, profile, gateways)
-func UserClaims(id string, scope []string) jwt.Claims {
+func UserClaims(username string, scopes []string) jwt.Claims {
 	return jwt.MapClaims{
 		"iss":             Issuer,
-		"sub":             id,
+		"sub":             username,
 		"type":            "user",
 		"interchangeable": true,
-		"scope":           scope,
+		"scope":           scopes,
 	}
 }
 
 // UserToken creates user claims for top level scopes (like apps, profile, gateways)
-func UserToken(id string, scope []string) string {
-	return TokenFromClaims(UserClaims(id, scope))
+func UserToken(username string, scopes []string) string {
+	return TokenFromClaims(UserClaims(username, scopes))
 }
 
 // DerivedUserClaims creates a user token with derived claims (like app:foo)
-func DerivedUserClaims(id string, apps map[string][]types.Right, gateways map[string][]types.Right, components map[string][]types.Right) jwt.Claims {
-	scope := make([]string, 0, len(apps)+len(gateways)+len(components))
+func DerivedUserClaims(username string, apps map[string][]types.Right, gateways map[string][]types.Right, components map[string][]types.Right) jwt.Claims {
+	scopes := make([]string, 0, len(apps)+len(gateways)+len(components))
 
 	for id := range apps {
-		scope = append(scope, "app:"+id)
+		scopes = append(scopes, scope.App(id))
 	}
 
 	for id := range gateways {
-		scope = append(scope, "gateway:"+id)
+		scopes = append(scopes, scope.Gateway(id))
 	}
 
-	for id := range gateways {
-		scope = append(scope, "component:"+id)
+	for id := range components {
+		scopes = append(scopes, scope.Component(id))
 	}
 
 	return jwt.MapClaims{
 		"iss":             Issuer,
-		"sub":             id,
+		"sub":             username,
 		"type":            "user",
 		"interchangeable": false,
-		"scope":           scope,
+		"scope":           scopes,
 		"apps":            apps,
 		"gateways":        gateways,
 		"components":      components,
@@ -53,6 +54,6 @@ func DerivedUserClaims(id string, apps map[string][]types.Right, gateways map[st
 }
 
 // DerivedUserToken creates a user token with derived claims (like app:foo)
-func DerivedUserToken(id string, apps map[string][]types.Right, gateways map[string][]types.Right, components map[string][]types.Right) string {
-	return TokenFromClaims(DerivedUserClaims(id, apps, gateways, components))
+func DerivedUserToken(username string, apps map[string][]types.Right, gateways map[string][]types.Right, components map[string][]types.Right) string {
+	return TokenFromClaims(DerivedUserClaims(username, apps, gateways, components))
 }
