aa676b61aa46f4c4538e38afc19b3d7021a4854e
Romeo Van Snick <romeovs@gmail.com>
2017-02-15 14:41:37 +0100

    Remove unused param

--- test/user.go
+++ test/user.go
@@ -9,7 +9,7 @@ import (
 )
 
 // UserClaims creates user claims for top level scopes (like apps, profile, gateways)
-func UserClaims(id string, username string, scope []string) jwt.Claims {
+func UserClaims(id string, scope []string) jwt.Claims {
 	return jwt.MapClaims{
 		"iss":             Issuer,
 		"sub":             id,
@@ -20,12 +20,12 @@ func UserClaims(id string, username string, scope []string) jwt.Claims {
 }
 
 // UserToken creates user claims for top level scopes (like apps, profile, gateways)
-func UserToken(id string, username string, scope []string) string {
-	return TokenFromClaims(UserClaims(id, username, scope))
+func UserToken(id string, scope []string) string {
+	return TokenFromClaims(UserClaims(id, scope))
 }
 
 // DerivedUserClaims creates a user token with derived claims (like app:foo)
-func DerivedUserClaims(id string, username string, apps map[string]types.Right, gateways map[string]types.Right, components map[string]types.Right) jwt.Claims {
+func DerivedUserClaims(id string, apps map[string]types.Right, gateways map[string]types.Right, components map[string]types.Right) jwt.Claims {
 	scope := make([]string, 0, len(apps)+len(gateways)+len(components))
 
 	for id := range apps {
@@ -53,6 +53,6 @@ func DerivedUserClaims(id string, username string, apps map[string]types.Right, 
 }
 
 // DerivedUserToken creates a user token with derived claims (like app:foo)
-func DerivedUserToken(id string, username string, apps map[string]types.Right, gateways map[string]types.Right, components map[string]types.Right) string {
-	return TokenFromClaims(DerivedUserClaims(id, username, apps, gateways, components))
+func DerivedUserToken(id string, apps map[string]types.Right, gateways map[string]types.Right, components map[string]types.Right) string {
+	return TokenFromClaims(DerivedUserClaims(id, apps, gateways, components))
 }
